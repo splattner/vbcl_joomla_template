863effd703cbcfc0c30ba1685524898c6e5a70cf
Sebastian Plattner <sebastian.plattner@gmail.com>
2016-04-11 22:32:08 +0200

    Facebook

--- html/bootstrap.vbcl.php
+++ html/bootstrap.vbcl.php
@@ -1,6 +1,15 @@
-  
+	  
 
-  
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/de_DE/sdk.js#xfbml=1&version=v2.5&appId=427775360718547";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+	
+
 	<!-- CONTENT -->
 	<div class="container whitebackground">
     	<div class="row">
